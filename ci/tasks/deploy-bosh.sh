ca39c376e9392a5e983d5d7bf2dd181ad21425ac
s-matyukevich <s.matyukevich@gmail.com>
2017-01-19 16:07:27 -0700

    deploy bosh script fixed

--- ci/tasks/deploy-bosh.sh
+++ ci/tasks/deploy-bosh.sh
@@ -10,18 +10,18 @@ if [ -n $nats_pass]; then
   nats="--nats-pwd $nats_pass"
 fi
 
-omg-cli/omg-linux photon   \   
-  --mode uaa   \   
-  --cidr $PCF_MANAGEMENT_CIDR   \   
-  --gateway $PCF_MANAGEMENT_GATEWAY   \   
-  --dns $PCF_MANAGEMENT_DNS
-  --bosh-private-ip $BOSH_IP   \   
-  --bosh-cpi-release-url "file://bosh-photon-cpi/release.tgz"   \   
-  --bosh-cpi-release-sha $sha1   \   
-  --director-name bosh-$DEPLOYMENT_NAME   \   
-  --photon-target $PHOTON_URL   \   
-  --photon-user $PHOTON_USER   \   
-  --photon-password $PHOTON_PASSWORD   \   
+omg-cli/omg-linux photon \
+  --mode uaa \
+  --cidr $PCF_MANAGEMENT_CIDR \
+  --gateway $PCF_MANAGEMENT_GATEWAY \
+  --dns $PCF_MANAGEMENT_DNS \
+  --bosh-private-ip $BOSH_IP \
+  --bosh-cpi-release-url "file://bosh-photon-cpi/release.tgz" \
+  --bosh-cpi-release-sha $sha1 \
+  --director-name bosh-$DEPLOYMENT_NAME \
+  --photon-target $PHOTON_URL \
+  --photon-user $PHOTON_USER \
+  --photon-password $PHOTON_PASSWORD \
   --photon-ignore-cert \
   --photon-project-id $PHOTON_PROJECT \
   --photon-network-id $PCF_MANAGEMENT_PHOTON_ID \
