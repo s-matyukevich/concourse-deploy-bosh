19ecf60a886078c8947fbcc8d0afdb38fd32224c
s-matyukevich <s.matyukevich@gmail.com>
2017-01-21 13:28:20 -0700

    ip generation algorithm fixed

--- ci/tasks/update-cf-deployment.sh
+++ ci/tasks/update-cf-deployment.sh
@@ -3,20 +3,15 @@
 all_ips=$(prips $(echo "$PCF_DEPLOYMENT_STATIC" | sed 's/-/ /'))
 IFS=$'\n'
 all_ips=($all_ips)
+delete=($HAPROXY_IP)
+array=( "${all_ips[@]/$delete}" )
 
 
-index=0
 get_ips(){
   res=""
-  for ((i = 0; i < $1; i++))
+  for ((i = $1; i <= $2; i++))
   do
-    if [ "${all_ips[$index]}" != "$HAPROXY_IP" ]
-    then
-      res="$res,${all_ips[$index]}"
-    else
-      i=$((i - 1))
-    fi
-    index=$((index + 1))
+    res="$res,${all_ips[$index]}"
   done
   echo "$res" | cut -c 2-
 }
@@ -63,38 +58,38 @@ vault-json-string: |
     "cc-vm-type": "large",
     "cc-worker-vm-type": "large",
     "clock-global-vm-type": "large",
-    "consul-ip": "$(get_ips 3)",
+    "consul-ip": "$(get_ips 0 2)",
     "consul-vm-type": "large",
     "diego-brain-disk-type": "51200",
-    "diego-brain-ip": "$(get_ips 3)",
+    "diego-brain-ip": "$(get_ips 3 5)",
     "diego-brain-vm-type": "large",
     "diego-cell-disk-type": "51200",
-    "diego-cell-ip": "$(get_ips 7)",
+    "diego-cell-ip": "$(get_ips 6 12)",
     "diego-cell-vm-type": "large",
-    "diego-db-ip": "$(get_ips 3)",
+    "diego-db-ip": "$(get_ips 13 15)",
     "diego-db-vm-type": "large",
     "deployment-name": "cf-nonprod",
-    "doppler-ip": "$(get_ips 3)",
+    "doppler-ip": "$(get_ips 16 18)",
     "doppler-vm-type": "large",
     "errand-vm-type": "large",
-    "etcd-machine-ip": "$(get_ips 3)",
+    "etcd-machine-ip": "$(get_ips 19 21)",
     "etcd-vm-type": "large",
     "haproxy-vm-type": "large",
     "haproxy-ip": "$HAPROXY_IP",
     "loggregator-traffic-controller-ip": "",
     "loggregator-traffic-controller-vmtype": "large",
     "mysql-disk-type": "51200",
-    "mysql-ip": "$(get_ips 3)",
-    "mysql-proxy-ip": "$(get_ips 3)",
+    "mysql-ip": "$(get_ips 22 24)",
+    "mysql-proxy-ip": "$(get_ips 25 27)",
     "mysql-proxy-vm-type": "large",
     "mysql-vm-type": "large",
-    "nats-machine-ip": "$(get_ips 3)",
+    "nats-machine-ip": "$(get_ips 28 30)",
     "nats-vm-type": "large",
     "nfs-allow-from-network-cidr": "",
     "nfs-disk-type": "51200",
-    "nfs-ip": "$(get_ips 3)",
+    "nfs-ip": "$(get_ips 31 33)",
     "nfs-vm-type": "large",
-    "router-ip": "$(get_ips 3)",
+    "router-ip": "$(get_ips 34 36)",
     "router-vm-type": "large",
     "uaa-vm-type": "large",
     "syslog-address": "$SYSLOG_ADDRESS"
