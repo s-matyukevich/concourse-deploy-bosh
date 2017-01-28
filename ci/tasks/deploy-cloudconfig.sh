810196c23e49904fabce30e498151ed9d1c019a3
s-matyukevich <s.matyukevich@gmail.com>
2017-01-27 17:17:34 -0700

    reserved range added in environment

--- ci/tasks/deploy-cloudconfig.sh
+++ ci/tasks/deploy-cloudconfig.sh
@@ -8,6 +8,10 @@ OMG_NETWORK_DNS_1=$PCF_MANAGEMENT_DNS
 OMG_NETWORK_DNS_2=$PCF_SERVICES_DNS
 OMG_NETWORK_DNS_3=$PCF_DEPLOYMENT_DNS
 
+OMG_NETWORK_RESERVED_1=$PCF_MANAGEMENT_RESERVED
+OMG_NETWORK_RESERVED_2=$PCF_SERVICES_RESERVED
+OMG_NETWORK_RESERVED_3=$PCF_DEPLOYMENT_RESERVED
+
 omg-cli/omg-linux register-plugin \
   --type cloudconfig \
   --pluginpath omg-cli/photon-cloudconfigplugin-linux
@@ -24,21 +28,17 @@ omg-cli/omg-linux deploy-cloudconfig \
   --network-az-1 az1 \
   --network-cidr-1 $PCF_MANAGEMENT_CIDR \
   --network-gateway-1 $PCF_MANAGEMENT_GATEWAY \
-  --network-reserved-1 $PCF_MANAGEMENT_RESERVED \
   --network-static-1 $PCF_MANAGEMENT_STATIC \
   --photon-network-name-1 $PCF_MANAGEMENT_PHOTON_ID \
   --network-name-2 pcf-services \
   --network-az-2 az1 \
   --network-cidr-2 $PCF_SERVICES_CIDR \
   --network-gateway-2 $PCF_SERVICES_GATEWAY \
-  --network-reserved-2 $PCF_SERVICES_RESERVED \
   --network-static-2 $PCF_SERVICES_STATIC \
   --photon-network-name-2 $PCF_SERVICES_PHOTON_ID \
   --network-name-3 pcf-deployment \
   --network-az-3 az1 \
   --network-cidr-3 $PCF_DEPLOYMENT_CIDR \
   --network-gateway-3 $PCF_DEPLOYMENT_GATEWAY \
-  --network-reserved-3 $PCF_DEPLOYMENT_RESERVED \
   --network-static-3 $PCF_DEPLOYMENT_STATIC \
   --photon-network-name-3 $PCF_DEPLOYMENT_PHOTON_ID 
-
