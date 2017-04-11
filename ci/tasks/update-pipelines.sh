87aca8bd84ed72410d5df10137ca2806692d412a
s-matyukevich <s.matyukevich@gmail.com>
2017-04-11 13:48:27 -0700

    cf deployment updated

--- ci/tasks/update-pipelines.sh
+++ ci/tasks/update-pipelines.sh
@@ -67,6 +67,7 @@ vault-hash-misc: secret/cf-$FOUNDATION_NAME-props
 vault-hash-password: secret/cf-$FOUNDATION_NAME-password
 vault-token: $VAULT_TOKEN
 uaa-ldap-password: $UAA_LDAP_PASSWORD
+pcf-services-static: $PCF_SERVICES_STATIC
 vault-json-string: |
   {
     "app-domain": "$APP_DOMAIN",
